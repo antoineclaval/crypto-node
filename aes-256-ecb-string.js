540ca449b085c200d7d4e24191a5439ce15e35b0
Antoine Claval <antoine@torsh.co>
2016-06-20 11:26:44 -0500

    add a ECB+IV method

---
+++ aes-256-ecb-string.js
@@ -0,0 +1,21 @@
+var crypto = require('crypto'),
+	// Fun quirk. We have to specify a IV even if ECB is not using IV at all.
+	// CreateCipher uses a password from which the key is derived 
+	// (the IV is derived as well, but that is ignored for ECB). The createCipheriv method however directly uses a key as bytes
+    iv = new Buffer(''),
+    // 32 * 8 Char = 256 bytes for the key
+    key = new Buffer('e6ee5c9847445e126008575068f1c4429ad34dbfcf7c675f6459c9a71919c0bc', 'hex'),
+    cipher = crypto.createCipheriv('aes-256-ecb', key, iv),
+    decipher = crypto.createDecipheriv('aes-256-ecb', key, iv),
+    chunks = [];
+
+cipher.setAutoPadding(true);
+var toEncrypt = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+var encryptedString = cipher.update( new Buffer(toEncrypt, 'utf8'),'buffer', 'hex');
+encryptedString += cipher.final('hex');
+console.log(toEncrypt,encryptedString);
+
+decipher.setAutoPadding(false);
+var decryptedString = decipher.update( encryptedString,'hex', 'buffer');
+decryptedString += decipher.final('hex');
+console.log(encryptedString,decryptedString);
